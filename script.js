289987d64803675e94627ee617435f1c3a78b1bd
Ismail <ismailmunyentwari9@gmail.com>
2023-03-04 13:02:28 +0200

    Add Remove elemrnt from both array and localstorage

---
+++ script.js
@@ -0,0 +1,62 @@
+const form = document.querySelector('form');
+const inputValue = document.getElementById('input');
+const listContainer = document.getElementById('list-container');
+
+let listArray=[];
+class ListObjects{
+    constructor(list,complete,id){
+       this.list=list;
+       this.complete=complete;
+       this.id=id;
+    }
+}
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const id = listArray.length+1;
+    const listObject = new ListObjects(inputValue.value,false,id);
+    listArray= [...listArray,listObject];
+    localStorage.setItem('listStorage', JSON.stringify(listArray));
+    UI.displayDatas();
+    UI.cleanInputs();
+  })
+  
+  class UI {
+    static displayDatas() {
+      let data =listArray.map((items) => {
+        return`<div id="lists" data-id=${items.id}>
+          <span><input class="form-check-input" type="checkbox"> <span>${items.list}</span></span>
+          <span><i class="fa fa-ellipsis-v"></i></span>
+        </div>`;
+      });
+      listContainer.innerHTML = data.join('');
+    }
+  
+    static cleanInputs(){
+      inputValue.value="";
+    }
+  }
+  
+  window.addEventListener('load',() =>{
+    if(localStorage.getItem('listStorage')){
+      listArray= JSON.parse(localStorage.getItem('listStorage'));
+      UI.displayDatas();
+    }
+  });
+  
+  listContainer.addEventListener('click', (e) =>{
+    if(e.target.classList.contains('fa-ellipsis-v')){
+      e.target.classList.remove('fa-ellipsis-v');
+      e.target.classList.add('fa-trash');
+      e.target.parentElement.parentElement.style.backgroundColor='#95577546';
+    }
+    else if (e.target.classList.contains('fa-trash')) {
+      const idValue = e.target.parentElement.parentElement.dataset.id;
+      const index = listArray.findIndex(item => item.id === parseInt(idValue));
+      listArray.splice(index, 1);
+      localStorage.setItem('listStorage', JSON.stringify(listArray));
+      UI.displayDatas();
+    }
+  })
+  
+  
